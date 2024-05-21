cd087469fe88a9c742471e3100a8ac90c931f103
Angel Velasquez <pcisavel@upc.edu.pe>
2024-05-21 15:09:17 -0500

    feat(publishing): added specific assets.

---
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Entities/ImageAsset.cs
@@ -0,0 +1,25 @@
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.ValueObjects;
+
+namespace ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Entities;
+
+public class ImageAsset : Asset
+{
+    public Uri? ImageUri { get; }
+
+    public override bool Readable => false;
+
+    public override bool Viewable => true;
+
+    public override string GetContent()
+    {
+        return ImageUri != null ? ImageUri.AbsoluteUri : string.Empty;
+    }
+
+    public ImageAsset() : base(EAssetType.Image) { }
+
+    public ImageAsset(string imageUrl) : base(EAssetType.Image)
+    {
+        ImageUri = new Uri(imageUrl);
+    }
+    
+}
---
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Entities/VideoAsset.cs
@@ -0,0 +1,21 @@
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.ValueObjects;
+
+namespace ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Entities;
+
+public class VideoAsset : Asset
+{
+    public Uri? VideoUri { get; private set; }
+
+    public override bool Readable => false;
+
+    public override bool Viewable => true;
+
+    public override string GetContent()
+    {
+        return VideoUri != null ? VideoUri.AbsoluteUri : string.Empty;
+    }
+
+    public VideoAsset() : base(EAssetType.Video) { VideoUri = null; }
+
+    public VideoAsset(string videoUrl) : base(EAssetType.Video) { VideoUri = new Uri(videoUrl); }
+}
