284f1ca1a261b20cb9f08e87acbedca74fa9d2cc
Angel Velasquez <pcisavel@upc.edu.pe>
2024-05-21 14:52:00 -0500

    feat(publishing): added category and generic asset.

---
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Entities/Asset.cs
@@ -0,0 +1,38 @@
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.ValueObjects;
+
+namespace ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Entities;
+
+public partial class Asset : IPublishable
+{
+    
+    public int Id { get; }
+    
+    public AcmeAssetIdentifier AssetIdentifier { get; private set; }
+    
+    public EPublishingStatus Status { get; protected set; }
+    
+    public EAssetType Type { get; private set; }
+
+    public virtual bool Readable => false;
+
+    public virtual bool Viewable => false;
+
+    public Asset(EAssetType type)
+    {
+        Type = type;
+        Status = EPublishingStatus.ReadyToEdit;
+        AssetIdentifier = new AcmeAssetIdentifier();
+    }
+    
+    public void SendToEdit() { Status = EPublishingStatus.ReadyToEdit; }
+
+    public void SendToApproval() { Status = EPublishingStatus.ReadyToApproval; }
+
+    public void ApproveAndLock() { Status = EPublishingStatus.ApprovedAndLocked; }
+
+    public void Reject() { Status = EPublishingStatus.Draft; }
+
+    public void ReturnToEdit() { Status = EPublishingStatus.ReadyToEdit; }
+
+    public virtual object GetContent() { return string.Empty; }
+}
---
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Entities/Category.cs
@@ -0,0 +1,22 @@
+using ACME.LearningCenterPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+using Microsoft.VisualBasic;
+
+namespace ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Entities;
+
+public class Category
+{
+    public Category(string name)
+    {
+        Name = name;
+    }
+
+    public Category()
+    {
+        Name = string.Empty;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    
+    
+}
