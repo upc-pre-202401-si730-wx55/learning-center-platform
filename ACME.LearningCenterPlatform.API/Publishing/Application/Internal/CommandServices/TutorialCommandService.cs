477fe9ecf209a579ae90b9430f19765cd7758bfa
Angel Velasquez <pcisavel@upc.edu.pe>
2024-05-24 07:47:00 -0500

    feat(publishing): added command and query services.

---
+++ ACME.LearningCenterPlatform.API/Publishing/Application/Internal/CommandServices/TutorialCommandService.cs
@@ -0,0 +1,41 @@
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Aggregates;
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Commands;
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Repositories;
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Services;
+using ACME.LearningCenterPlatform.API.Shared.Domain.Repositories;
+
+namespace ACME.LearningCenterPlatform.API.Publishing.Application.Internal.CommandServices;
+
+public class TutorialCommandService(ITutorialRepository tutorialRepository, IUnitOfWork unitOfWork) : ITutorialCommandService
+{
+    public async Task<Tutorial?> Handle(CreateTutorialCommand command)
+    {
+        var tutorial = new Tutorial(command.Title, command.Summary, command.CategoryId);
+        try
+        {
+            await tutorialRepository.AddAsync(tutorial);
+            await unitOfWork.CompleteAsync();
+            return tutorial;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public async Task<Tutorial?> Handle(AddVideoAssetToTutorialCommand command)
+    {
+        var tutorial = await tutorialRepository.FindByIdAsync(command.TutorialId);
+        if (tutorial is null) throw new Exception("Tutorial not found");
+        try
+        {
+            // tutorial.AddVideo(command.VideoUrl());
+            await unitOfWork.CompleteAsync();
+            return tutorial;
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
---
+++ ACME.LearningCenterPlatform.API/Publishing/Application/Internal/QueryServices/TutorialQueryService.cs
@@ -0,0 +1,24 @@
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Aggregates;
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Queries;
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Repositories;
+using ACME.LearningCenterPlatform.API.Publishing.Domain.Services;
+
+namespace ACME.LearningCenterPlatform.API.Publishing.Application.Internal.QueryServices;
+
+public class TutorialQueryService(ITutorialRepository tutorialRepository) : ITutorialQueryService
+{
+    public async Task<Tutorial?> Handle(GetTutorialByIdQuery query)
+    {
+        return await tutorialRepository.FindByIdAsync(query.TutorialId);
+    }
+
+    public async Task<IEnumerable<Tutorial>> Handle(GetAllTutorialsQuery query)
+    {
+        return await tutorialRepository.ListAsync();
+    }
+
+    public async Task<IEnumerable<Tutorial>> Handle(GetAllTutorialsByCategoryIdQuery query)
+    {
+        return await tutorialRepository.FindByCategoryId(query.CategoryId);
+    }
+}
