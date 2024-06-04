f511a9163d34d90469ca0abab95dcb499461a594
Angel Velasquez <pcisavel@upc.edu.pe>
2024-06-04 15:45:59 -0500

    feat(iam): implemented interface layer resources and assemblers.

---
+++ ACME.LearningCenterPlatform.API/IAM/Interfaces/REST/Transform/SignUpCommandFromResourceAssembler.cs
@@ -0,0 +1,12 @@
+using ACME.LearningCenterPlatform.API.IAM.Domain.Model.Commands;
+using ACME.LearningCenterPlatform.API.IAM.Interfaces.REST.Resources;
+
+namespace ACME.LearningCenterPlatform.API.IAM.Interfaces.REST.Transform;
+
+public static class SignUpCommandFromResourceAssembler
+{
+    public static SignUpCommand ToCommandFromResource(SignUpResource resource)
+    {
+        return new SignUpCommand(resource.Username, resource.Password);
+    }
+}
